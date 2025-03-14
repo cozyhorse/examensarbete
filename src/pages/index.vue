4f142c308cb0024f99bb90241703a555985af875
cozyhorse <100480213+cozyhorse@users.noreply.github.com>
2025-03-14 13:46:23 +0100

    made index more responsive

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card class="main-card" width="1650" height="900" elevation="5">
+  <v-card class="main-card" elevation="5">
     <v-container class="customContainer d-flex justify-center align-center" fluid>
         <v-row justify="center" no-gutters>
             <v-btn class="custom-btn" width="300" height="55" @click="onClick"></v-btn>
@@ -18,15 +18,31 @@ const onClick = () => {
 </script>
 
 <style scoped lang="scss">
-.customContainer{
-  height:1000px ;
+.customContainer {
+  height: 86vh;
+  width: 100%;
   background-image: url("/src/assets/map/Level_0/_composite.png");
   background-position: center center;
+  background-size: 4000px;
   overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-.main-card{
-margin: auto;
+
+.main-card {
+  width: 100%;
+  height: 86vh;
+  max-width: 1650px;
+  max-height: 900px;
+  aspect-ratio: 16 / 9;
+  margin: auto;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
+
 .custom-btn{
   background: none;
   background-image: url("/src/assets/start.gif");
@@ -37,7 +53,7 @@ margin: auto;
 }
 
 ::v-deep(.v-btn--variant-elevated){
-  box-shadow: none;
+  box-shadow: none !important;
 }
 
 </style>
